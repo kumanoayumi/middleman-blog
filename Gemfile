6e7cf570601725522dd71d5420c71e791b0edd9a
kumano ayumi <kumayuzuki@gmail.com>
2013-04-20 00:19:21 +0900

    Add middleman-deploy config

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gem "middleman-blog", "~> 3.1.1"
 
 # For feed.xml.builder
 gem "builder", "~> 3.0.0"
+gem "middleman-deploy", "~> 0.0.11"
--- config.rb
+++ config.rb
@@ -106,3 +106,7 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+end
